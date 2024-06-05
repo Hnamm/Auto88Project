9e2489dd825d34fe597b392e4a62a4121e966a82
Tuandtit <tuandtit6326@gmail.com>
2024-06-05 23:29:01 +0700

    First Commit

--- src/main/java/Entities/TaiKhoan.java
+++ src/main/java/Entities/TaiKhoan.java
@@ -9,5 +9,47 @@ package Entities;
  * @author admin
  */
 public class TaiKhoan {
+    private int id;
+    private String username;
+    private String password;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public TaiKhoan(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
 }
